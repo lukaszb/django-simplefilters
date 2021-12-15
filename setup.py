7fece576d8080f057fa971b9821e35b0765f853d
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2021-12-15 21:30:00 +0100

    Added long description when distributing package

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+import os
+
+
+README = os.path.join(os.path.dirname(__file__), 'README.md')
 
 
 setup(
@@ -7,6 +11,8 @@ setup(
     url='https://github.com/lukaszb/django-simplefilters',
     license='MIT',
     description='Simple drop-in replacement for rest_framework.filters',
+    long_description=open(README).read(),
+    long_description_content_type='text/markdown',
     author='Lukasz Balcerzak',
     author_email='lukaszbalcerzak@gmail.com',
     zip_safe=False,
